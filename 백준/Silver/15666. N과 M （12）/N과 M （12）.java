547985e4099194341e4bdf8eab511b418db6eca0
SeungHuiHan <98226400+SeungHuiHan@users.noreply.github.com>
2024-11-16 21:42:05 +0900

    [Silver II] Title: N과 M (12), Time: 184 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Main{
+    private static int[] numbers;
+    private static int[] arr;
+    private static int N,M;
+    private static StringBuilder sb;
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        sb=new StringBuilder();
+
+        N=sc.nextInt();
+        M=sc.nextInt();
+        sc.nextLine();
+        numbers=new int[M];
+        arr=new int[N];
+
+        for(int i=0;i<N;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        backtracking(0,0);
+        System.out.println(sb.toString());
+
+    }
+
+    private static void backtracking(int start,int count){
+        if(count==M){
+            for(int n:numbers){
+                sb.append(n).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int tmp=-1;
+        for(int i=start;i<N;i++){
+            if(tmp!=arr[i]){
+                tmp=arr[i];
+                numbers[count]=arr[i];
+                backtracking(i,count+1);
+            }
+
+        }
+    }
+}
