156968c5856bd0d643b04bccad4ec70a8dfa98b0
SeungHuiHan <98226400+SeungHuiHan@users.noreply.github.com>
2025-02-14 12:29:35 +0900

    [Gold III] Title: 크게 만들기, Time: 568 ms, Memory: 44136 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.java
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -4,39 +4,25 @@ public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         StringBuilder sb=new StringBuilder();
-        Stack<Integer> stack=new Stack<>();
+
+       //char는 정수 비교가 가능 ('1' < '2' → true)
+        Stack<Character> stack=new Stack<>();
 
         int N=sc.nextInt();
         int K=sc.nextInt();
         sc.nextLine();
 
 
-        String[] input=sc.nextLine().split("");
+        String input=sc.nextLine();
         int count=0;
-        for(int i=0;i<N;i++){
-            int a=Integer.parseInt(input[i]);
-
-            if(count<K){
-                if(stack.isEmpty()){
-                    stack.push(a);
-                }
-                else{
-                    int len=stack.size();
-                    for(int j=0;j<len;j++){
-                        if(count<K && stack.peek()<a){
-                            stack.pop();
-                            count++;
-                        }else break;
-                    }
-                    stack.push(a);
-
-                }
-            }else{
-                stack.push(a);
+        for(char c:input.toCharArray()){
+            while(!stack.isEmpty() && count<K && stack.peek()<c){
+                stack.pop();
+                count++;
             }
-
+            stack.push(c);
         }
-
+       
         for(int i=0;i<N-K;i++){
             sb.append(stack.get(i));
         }
