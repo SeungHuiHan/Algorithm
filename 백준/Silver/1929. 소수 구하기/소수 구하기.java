c92a2ef705ce078e5093c504e8fef37d5d2b0f5f
SeungHuiHan <98226400+SeungHuiHan@users.noreply.github.com>
2025-04-30 19:18:44 +0900

    [Silver III] Title: 소수 구하기, Time: 316 ms, Memory: 23276 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        int M=sc.nextInt();
+        int N=sc.nextInt();
+
+        boolean[] isPrime=new boolean[N+1];
+        Arrays.fill(isPrime,true);
+        isPrime[1]=false;
+
+        for(int i=2;i*i<=N;i++){
+            if(isPrime[i]){
+                for(int j=i*i;j<=N;j+=i){
+                    isPrime[j]=false;
+                }
+            }
+        }
+
+
+        StringBuilder sb=new StringBuilder();
+        for(int i=M;i<=N;i++){
+            if(isPrime[i]){
+                sb.append(i).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
