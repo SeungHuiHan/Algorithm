15624c32e9601b27d17801b6885abf22e0fcdf60
SeungHuiHan <98226400+SeungHuiHan@users.noreply.github.com>
2025-06-07 23:53:32 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 196 ms, Memory: 25476 KB, Score: 201 point -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        long[] dp=new long[n+1];
+
+        if(n==0){
+            System.out.println(0);
+            return;
+        }
+        dp[0]=0;
+        
+        if(n==1){
+            System.out.println(1);
+            return;
+        }
+        dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            dp[i]=(dp[i-1]+dp[i-2])%1000000007;
+        }
+
+        System.out.println(dp[n]);
+
+    }
+}
