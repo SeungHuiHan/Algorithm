52f0e6373760f114b0be6b7c45fe0710da3b5dd9
SeungHuiHan <98226400+SeungHuiHan@users.noreply.github.com>
2025-06-19 01:47:04 +0900

    [Silver I] Title: 1, 2, 3 더하기 9, Time: 180 ms, Memory: 23124 KB -BaekjoonHub

--- 백준/Silver/16195. 1， 2， 3 더하기 9/1， 2， 3 더하기 9.java
+++ 백준/Silver/16195. 1， 2， 3 더하기 9/1， 2， 3 더하기 9.java
@@ -5,47 +5,42 @@ import java.io.*;
 public class Main{
     public static void main(String[] args)throws IOException {
         BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb=new StringBuilder();
-
-         int T = Integer.parseInt(bf.readLine());
-        int[][] arr = new int[T][2];
-        int maxN = 0;
-        int maxM = 0;
-
-        for(int i = 0; i < T; i++){
-            st = new StringTokenizer(bf.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken()); // n
-            arr[i][1] = Integer.parseInt(st.nextToken()); // m
-            maxN = Math.max(maxN, arr[i][0]);
-            maxM = Math.max(maxM, arr[i][1]);
-        }
-
-        // dp[i][j] = i를 j개의 수로 만드는 방법의 수
-        long[][] dp = new long[maxN+1][maxM+1];
-        dp[0][0] = 1;
-
-        for(int i = 1; i <= maxN; i++){
-            for(int j = 1; j <= maxM; j++){
-                for(int k = 1; k <= 3; k++){
-                    if(i - k >= 0){
-                        dp[i][j] = (dp[i][j] + dp[i - k][j - 1]) % 1000000009;
-                    }
-                }
-            }
-        }
-
-        for(int i = 0; i < T; i++){
-            int n = arr[i][0];
-            int m = arr[i][1];
-            long result = 0;
-            for(int j = 1; j <= m; j++){
-                result = (result + dp[n][j]) % 1000000009;
-            }
-            sb.append(result).append("\n");
-        }
+        StringTokenizer st;
         
+        int T=Integer.parseInt(bf.readLine());
+        int[][] arr=new int[T][2];
+        int max=0;
 
+        for(int i=0;i<T;i++){
+            st=new StringTokenizer(bf.readLine());
+            arr[i][0]=Integer.parseInt(st.nextToken());
+            arr[i][1]=Integer.parseInt(st.nextToken());
+            max=Math.max(max,arr[i][0]);
+        }
+
+        long[][] dp=new long[max+1][max+1];
+        dp[0][0]=1;
+
+        for(int i=1;i<=max;i++){
+            for(int j=1;j<=i;j++){
+                
+                if(i - 1 >= 0) dp[i][j] = (dp[i][j] + dp[i - 1][j - 1])%1000000009;
+                if(i - 2 >= 0) dp[i][j] = (dp[i][j] + dp[i - 2][j - 1])%1000000009;
+                if(i - 3 >= 0) dp[i][j] = (dp[i][j] + dp[i - 3][j - 1])%1000000009;
+
+            }
+        }
+
+        for(int i=0;i<T;i++){
+            int a=arr[i][0];
+            int b=arr[i][1];
+            long sum=0;
+            for(int j=0;j<=b;j++){
+                sum=(sum+dp[a][j])%1000000009;
+            }
+            sb.append(sum).append("\n");
+        }
         System.out.println(sb);
     }
 }
