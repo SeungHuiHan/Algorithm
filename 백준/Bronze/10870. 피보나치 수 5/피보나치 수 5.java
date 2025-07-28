c74c6d73ef0c9cdf0c83acf5a1ca95bca3078f57
SeungHuiHan <98226400+SeungHuiHan@users.noreply.github.com>
2025-07-28 23:17:39 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 180 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        
+        int n=sc.nextInt();
+        int[] dp=new int[n+1];
+       
+        if(n==0){
+            System.out.println(0);
+            return;
+        }
+        dp[0]=0;
+        
+        if(n==1){
+            System.out.println(1);
+            return;
+        }
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        System.out.println(dp[n]);
+    }
+}
