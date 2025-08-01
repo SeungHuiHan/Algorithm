1861ae3b9c8cf90e88765ea56a69b25a38eacd46
SeungHuiHan <98226400+SeungHuiHan@users.noreply.github.com>
2025-08-01 23:43:42 +0900

    [Bronze III] Title: 이진수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3460. 이진수/이진수.py
+++ 백준/Bronze/3460. 이진수/이진수.py
@@ -3,13 +3,10 @@ T=int(input())
 for _ in range(T):
     n=int(input())
     
-    binary=[]
-    while n>0:
-        binary.append(n%2)
-        n=n//2
-        
-    for i in range(len(binary)):
-        if binary[i]!=0:
+    b=bin(n)[2:]
+    
+    for i in range(len(b)):
+        if b[-(i+1)]=='1':
             print(i,end=" ")
             
     print()
