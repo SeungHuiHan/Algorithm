5e9e8a0df44a297d368983d425d19b05fb0f6c39
SeungHuiHan <98226400+SeungHuiHan@users.noreply.github.com>
2025-08-13 21:38:45 +0900

    [Silver IV] Title: 국영수, Time: 2844 ms, Memory: 83764 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -2,5 +2,8 @@ N=int(input())
 arr=[list(map(str,input().split())) for _ in range(N)]
 arr.sort(key=lambda x:(-int(x[1]),int(x[2]),-int(x[3]),x[0]))
 
+lines=[]
 for name in arr:
-    print(name[0])
+    lines.append(name[0]+"\n")
+answer=''.join(lines)
+print(answer)
