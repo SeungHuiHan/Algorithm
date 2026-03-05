b81ef8b0a712c6878b84a565fa650b97182e77c4
SeungHuiHan <98226400+SeungHuiHan@users.noreply.github.com>
2026-03-06 00:58:43 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        
+        long n=num;
+        while(answer<500 && n!=1){
+            if(n%2==0) n=n/2;
+            else n=n*3+1;
+            
+            answer++;
+        }
+        
+        if(answer>=500) answer=-1;
+        return answer;
+    }
+}
