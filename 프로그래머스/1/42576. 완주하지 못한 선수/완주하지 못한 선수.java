5463634597bcdaa271e573e18c8ea91475e0666a
SeungHuiHan <98226400+SeungHuiHan@users.noreply.github.com>
2026-03-10 01:06:55 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.36 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,20 +3,26 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        HashMap<String,Integer> map=new HashMap<>();
-        
-        for(int i=0;i<completion.length;i++){
-            map.put(completion[i],map.getOrDefault(completion[i],0)+1);
+        HashMap<String,Integer> pmap=new HashMap<>();
+        HashMap<String,Integer> cmap=new HashMap<>();
+        for(String p:participant){
+            if(pmap.containsKey(p)){
+                pmap.put(p,pmap.get(p)+1);
+            }else
+                pmap.put(p,1);
         }
         
-        for(int i=0;i<participant.length;i++){
-            
-            if(!map.containsKey(participant[i]) || map.get(participant[i])==0){
-                return  participant[i];
-            }
-            
-            map.put(participant[i],map.get(participant[i])-1);
+        for(String c:completion){
+            if(cmap.containsKey(c)){
+                cmap.put(c,cmap.get(c)+1);
+            }else
+                cmap.put(c,1);
         }
-        return "";
+        
+        for(String p:participant){
+            if(!cmap.containsKey(p)) return p;
+            if(pmap.get(p)>cmap.get(p)) return p;
+        }
+        return answer;
     }
 }
