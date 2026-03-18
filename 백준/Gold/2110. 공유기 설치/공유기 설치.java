f1e3716ff2e1a002fc09b5804f490f6f2eb4f5b7
SeungHuiHan <98226400+SeungHuiHan@users.noreply.github.com>
2026-03-18 21:08:12 +0900

    [Gold IV] Title: 공유기 설치, Time: 1020 ms, Memory: 175972 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,51 +1,52 @@
 import java.util.*;
-
+//최소의 최대
 public class Main{
+    
     private static int N,C;
-    private static int[] homes;
-
+    private static int[] arr;
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-
+        int answer=0;
         N=sc.nextInt();
         C=sc.nextInt();
-
-        homes=new int[N];
+        
+        arr=new int[N];
         for(int i=0;i<N;i++){
-            homes[i]=sc.nextInt();
+            arr[i]=sc.nextInt();
         }
-
-        Arrays.sort(homes);
-
-        int left=1;
-        int right=homes[N-1]-homes[0];
-        int answer=0; //공유기 가장 인접한 거리의 최대값
-
+        
+        Arrays.sort(arr);
+        
+        int left=0;
+        int right=1000000000;
+        
         while(left<=right){
-            int mid=(left+right)/2;
-
-            if(canPlace(mid,homes)){
-                answer=mid;
+            int mid=(left+right)/2; //거리
+            
+            if(canCut(mid)){
                 left=mid+1;
+                answer=mid;
             }else{
                 right=mid-1;
             }
         }
-
+        
         System.out.println(answer);
+        
+        
     }
-
-    private static boolean canPlace(int mid,int[] homes){
-        int count=1; //homes[0]에 하나 설치
-        int lastHome=homes[0];
-
-        for(int i=1;i<homes.length;i++){
-            if(homes[i]-lastHome>=mid){ //mid 는 가장 인접한 공유기 거리의 최대값
+    
+    private static boolean canCut(int mid){
+        int count=1; //최대 공유기 개수
+        int prev=arr[0];
+        
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]-prev>=mid){
                 count++;
-                lastHome=homes[i];
-            }
+                 prev=arr[i];
+            } 
         }
-
-        return count>=C;
+        
+        return count>=C?true:false;
     }
 }
