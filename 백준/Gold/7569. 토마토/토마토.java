81c46d2a1d3a065ce5a3726ec01eb013c0ea6274
SeungHuiHan <98226400+SeungHuiHan@users.noreply.github.com>
2026-03-28 23:57:45 +0900

    [Gold V] Title: 토마토, Time: 1244 ms, Memory: 173976 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,83 +1,86 @@
 import java.util.*;
 
 public class Main{
-    private static ArrayDeque<int []> queue;
-    private static int M,N,H;
-    private static int[][][] box;
-    private static int maxCount=0;
-
-    private static int[] dx={0,0,-1,1,0,0};
-    private static int[] dy={0,0,0,0,-1,1};
+    private static int[] dx={0,0,0,0,-1,1};
+    private static int[] dy={0,0,-1,1,0,0};
     private static int[] dz={-1,1,0,0,0,0};
-
-
+    private static int[][][] maps;
+    private static ArrayDeque<int[]> dq=new ArrayDeque<>();
+    private static int N,M,H;
+    private static boolean cooked;
+    
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-
         M=sc.nextInt();
         N=sc.nextInt();
         H=sc.nextInt();
-
-        box=new int[H][N][M];
-
-        queue=new ArrayDeque<>();
-
+        sc.nextLine();
+        
+        maps=new int[H][N][M];
+        
         for(int h=0;h<H;h++){
-            for(int n=0;n<N;n++){
-                for(int m=0;m<M;m++){
-                    box[h][n][m] = sc.nextInt();
-
-                    if(box[h][n][m]==1){ //익은 토마토 큐에 추가
-                        queue.add(new int[]{h,n,m});
+            
+            for(int i=0;i<N;i++){
+                String[] s=sc.nextLine().split(" ");
+                for(int j=0;j<M;j++){
+                    
+                    maps[h][i][j]=Integer.parseInt(s[j]);
+                
+                    if(maps[h][i][j]==1){
+                         dq.add(new int[]{h,i,j});
                     }
                 }
+                
             }
         }
-
-
         bfs();
-
+        
+        int max=0;
         for(int h=0;h<H;h++){
-            for(int n=0;n<N;n++){
-                for(int m=0;m<M;m++){
-                    if(box[h][n][m]==0){
-                        System.out.println(-1); //처음에 모든 토마토가 익지는 못한 상황
+             for(int i=0;i<N;i++){
+                for(int j=0;j<M;j++){
+               
+                    if(maps[h][i][j]==0){
+                        System.out.println(-1);
                         return;
                     }
-
-                    maxCount=Math.max(maxCount,box[h][n][m]);
+                    
+                    max=Math.max(max,maps[h][i][j]);
                 }
+               
+                
             }
         }
-
-
-        System.out.println(maxCount-1); // 처음에 모든 토마토가 다 익은 상황이면 maxCount=1이다.
-
-
-
+        
+        if(!cooked) System.out.println(0);
+        else System.out.println(max-1);
+        
     }
-
+    
     private static void bfs(){
-        while(!queue.isEmpty()){
-            int[] current=queue.poll();
+        while(!dq.isEmpty()){
+            int[] current=dq.poll();
             int cz=current[0];
-            int cy=current[1];
-            int cx=current[2];
-
+            int cx=current[1];
+            int cy=current[2];
+            
+            
             for(int i=0;i<6;i++){
-                int nz=cz+dz[i];
-                int ny=cy+dy[i];
                 int nx=cx+dx[i];
-
-
-                if(nz>=0 && ny>=0 && nx>=0 && nz<H && ny<N && nx<M && box[nz][ny][nx]==0) {
-                    box[nz][ny][nx]=box[cz][cy][cx]+1;
-                    queue.add(new int[]{nz,ny,nx});
+                int ny=cy+dy[i];
+                int nz=cz+dz[i];
+               
+                if(nx<0 || ny<0|| nz<0 ||
+                   nx>=N || ny>=M || nz>=H|| 
+                   maps[nz][nx][ny]==-1) continue;
+                
+                if(maps[nz][nx][ny]==0){
+                    cooked=true;
+                    maps[nz][nx][ny]=maps[cz][cx][cy]+1;
+                    dq.add(new int[]{nz,nx,ny});
                 }
-
+                
             }
-
         }
-
     }
 }
