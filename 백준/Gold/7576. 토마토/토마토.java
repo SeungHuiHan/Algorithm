58030c3adbc6daf785a078a71cdbbfe5ccba4e06
SeungHuiHan <98226400+SeungHuiHan@users.noreply.github.com>
2026-03-28 21:10:53 +0900

    [Gold V] Title: 토마토, Time: 832 ms, Memory: 136880 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,63 +1,63 @@
 import java.util.*;
 
 public class Main{
-    private static ArrayDeque<int []> queue;
-    private static int M,N;
-    private static int[][] box;
-    private static int maxCount=0;
-    private static int[] dx={-1,1,0,0};
-    private static int[] dy={0,0,-1,1};
-
+    private static int[] dx={0,0,-1,1};
+    private static int[] dy={-1,1,0,0};
+    private static int[][] maps;
+    private static ArrayDeque<int[]> dq=new ArrayDeque<>();
+    private static int N,M;
+    
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-
         M=sc.nextInt();
         N=sc.nextInt();
+        sc.nextLine();
         
-        box=new int[N][M];
-        queue=new ArrayDeque<>();
+        maps=new int[N][M];
         for(int i=0;i<N;i++){
+            String[] s=sc.nextLine().split(" ");
             for(int j=0;j<M;j++){
-                box[i][j]=sc.nextInt();
-                if(box[i][j]==1){
-                    queue.add(new int[]{i,j});
+                maps[i][j]=Integer.parseInt(s[j]);
+                
+                if(maps[i][j]==1){
+                     dq.add(new int[]{i,j});
                 }
             }
         }
-
         bfs();
-
+        
+        int max=0;
         for(int i=0;i<N;i++){
             for(int j=0;j<M;j++){
-
-                if(box[i][j]==0){
-                    System.out.println(-1); //처음에 모든 토마토가 익지는 못한 상황
+                if(maps[i][j]==0){
+                    System.out.println(-1);
                     return;
-                }else{
-                    maxCount=Math.max(maxCount,box[i][j]);
                 }
-
+                max=Math.max(max,maps[i][j]);
             }
         }
+        System.out.println(max-1);
         
-        // 결과 출력 (최소 일수는 1부터 시작하므로 -1)
-        System.out.println(maxCount-1);
     }
-
+    
     private static void bfs(){
-        while(!queue.isEmpty()){
-            int[] current=queue.poll();
+        while(!dq.isEmpty()){
+            int[] current=dq.poll();
             int cx=current[0];
             int cy=current[1];
-
+        
+            
             for(int i=0;i<4;i++){
                 int nx=cx+dx[i];
                 int ny=cy+dy[i];
-
-                if(nx>=0 && ny>=0 && nx<N && ny<M && box[nx][ny]==0 ){
-                    box[nx][ny]=box[cx][cy]+1;
-                    queue.add(new int[]{nx,ny});
+               
+                if(nx<0 || ny<0|| nx>=N || ny>=M || maps[nx][ny]==-1) continue;
+                
+                if(maps[nx][ny]==0){
+                    maps[nx][ny]=maps[cx][cy]+1;
+                    dq.add(new int[]{nx,ny});
                 }
+                
             }
         }
     }
